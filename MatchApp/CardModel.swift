ad38a88b9b6513d1fdd2d9893a24b437c269afcb
Brian Dilts <brianedilts@gmail.com>
2021-05-07 16:37:57 -0600

    - add assets to app - add CardModel to app - force app to work in landscape - add beginning elements to UIView

---
+++ MatchApp/CardModel.swift
@@ -0,0 +1,19 @@
+//
+//  CardModel.swift
+//  MatchApp
+//
+//  Created by Brian Dilts on 5/7/21.
+//
+
+import Foundation
+
+class CardModel {
+    
+    func getCards() {
+        
+        
+        
+    }
+    
+}
+
