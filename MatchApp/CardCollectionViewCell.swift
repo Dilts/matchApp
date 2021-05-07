32f7c8c437372aaf73cb9ebee236d2ae1986de82
Brian Dilts <brianedilts@gmail.com>
2021-05-07 17:46:19 -0600

    - add cocoa touch file for each card cell - connect the cells to CardCollectionViewCell

---
+++ MatchApp/CardCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CardCollectionViewCell.swift
+//  MatchApp
+//
+//  Created by Brian Dilts on 5/7/21.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    
+    
+}
