d3a8e69353808ba5445100fa7e58e30a38b17f53
Brian Dilts <brianedilts@gmail.com>
2021-05-07 18:25:31 -0600

    - add delegate and datasource to ViewController

--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     let model = CardModel()
     var cardsArray = [Card]()
     
@@ -18,8 +20,33 @@ class ViewController: UIViewController {
         
         cardsArray = model.getCards()
         
+        // Set the ViewController as the datasource and the delegate of the collection view
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
     }
 
+    //MARK: - Collection View Delegate Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // Return number of cards
+        return cardsArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // Get a cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        //TODO: Finishing configuring cell
+        
+        
+        // Return it
+        return cell
+    }
+
+
+    
 
 }
 
